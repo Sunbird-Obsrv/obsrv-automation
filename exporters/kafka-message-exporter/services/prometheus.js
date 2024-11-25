cc0912a990066246384d8d8b42ba53bd741ef6ff
SurabhiAngadi <surabhia@sanketika.in>
2024-11-25 16:35:14 +0530

    feat: removed debug logs

--- exporters/kafka-message-exporter/services/prometheus.js
+++ exporters/kafka-message-exporter/services/prometheus.js
@@ -11,7 +11,6 @@ register.setDefaultLabels(config.defaultLabels);
 const metrics = {};
 
 const registerMetric = (metric) => {
-    console.log("Registering metric")
     register.registerMetric(metric);
 }
 
@@ -21,28 +20,23 @@ const createGuageMetric = payload => {
 }
 
 const getMetric = key => {
-    console.log(`Get metric ${metrics}, ${key}`)
     return _.get(metrics, key);
 }
 
 // Function to get or create a metric dynamically
 const getOrCreateMetric = (key, labels) => {
-    console.log("Get or create a metric")
     const metricName = renameKey(key)
-    console.log("metricName: ", metricName)
     let metric = getMetric(metricName);
     if (!metric) {
         metric = createGuageMetric({ name: metricName, labelNames: Object.keys(labels) });
         registerMetric(metric);
         metrics[metricName] = metric;
     }
-    console.log("Metric: ", metric)
     return metric;
 };
 
 // Transforms list of metric labels array {key: string, value: string}[] into a map {key1: value1, key2: value2}
 const getLabelsMapping = (labels) => {
-    console.log("Labels: ", labels)
     return _.reduce(labels, (mapping, current) => {
         const { key, value } = current;
         mapping[key] = value;
